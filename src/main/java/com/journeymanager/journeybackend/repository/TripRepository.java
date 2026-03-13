9dbc9476db95ab976a0a77bbaad7064b3c7c4705
JourneyManager <yklee88@hotmail.com>
2026-03-13 09:02:33 +0800

    Add analytics count queries to TripRepository

--- src/main/java/com/journeymanager/journeybackend/repository/TripRepository.java
+++ src/main/java/com/journeymanager/journeybackend/repository/TripRepository.java
@@ -7,7 +7,9 @@ import java.util.List;
 import java.util.Optional;
 
 public interface TripRepository extends JpaRepository<Trip, Long> {
+    long countByTenantId(Long tenantId);
 
+    long countByTenantIdAndStatus(Long tenantId, TripStatus status);
     List<Trip> findByTenantId(Long tenantId);
 
     Optional<Trip> findByIdAndTenantId(Long id, Long tenantId);
